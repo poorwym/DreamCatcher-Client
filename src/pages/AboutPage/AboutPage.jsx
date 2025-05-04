fb5021b25f71dbdf31930681424b05c58395a6ca
yilia-ryu <wuyiliawyr@outlook.com>
2025-05-04 12:43:21 +0800

    :

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -3,8 +3,20 @@ import React from 'react'
 function AboutPage() {
   return (
     <div>
-      <h1>About</h1>
-    </div>
+    <header>
+      <h1>关于我们</h1>
+    </header>
+    <section className="about">
+      <img alt="我们的团队" />
+      <div className="content">
+        <h2>我们的使命</h2>
+        <p>我们致力于用技术改变世界，创造更美好的未来。我们的团队由一群充满激情的开发者、设计师和产品经理组成。</p>
+      </div>
+    </section>
+    <footer>
+      <p>&copy; 2025 DreamCatcher. 版权所有。</p>
+    </footer>
+  </div>
   )
 }
 
