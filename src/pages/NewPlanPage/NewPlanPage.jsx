aa106768c26e10e9da553cb488dda5efa12809ac
AlexWu <wuy691128@gmail.com>
2025-05-04 15:29:14 +0800

    upload example

--- src/pages/NewPlanPage/NewPlanPage.jsx
+++ src/pages/NewPlanPage/NewPlanPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NewPlanPage() {
   return (
     <div>
-      
+      <h1>New Plan</h1>
     </div>
   )
 }
--- src/pages/PlanMap3DPage/PlanMap3DPage.jsx
+++ src/pages/PlanMap3DPage/PlanMap3DPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PlanMap3DPage() {
   return (
     <div>
-      
+      <h1>Plan Map 3D</h1>
     </div>
   )
 }
